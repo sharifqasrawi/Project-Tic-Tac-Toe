a3c4af06fcddb378e6e58004df603d783f321a6a
Sharif Qasrawi <sherifqasrawi@windowslive.com>
2019-03-29 21:08:47 +0100

    First Commit

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sq9 = new System.Windows.Forms.Label();
+            this.sq8 = new System.Windows.Forms.Label();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.sq6 = new System.Windows.Forms.Label();
+            this.sq5 = new System.Windows.Forms.Label();
+            this.sq4 = new System.Windows.Forms.Label();
+            this.sq3 = new System.Windows.Forms.Label();
+            this.sq2 = new System.Windows.Forms.Label();
+            this.sq7 = new System.Windows.Forms.Label();
+            this.sq1 = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.oplayer = new System.Windows.Forms.RadioButton();
+            this.xplayer = new System.Windows.Forms.RadioButton();
+            this.box = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sq9
+            // 
+            this.sq9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sq9.Enabled = false;
+            this.sq9.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.sq9.Location = new System.Drawing.Point(322, 162);
+            this.sq9.Name = "sq9";
+            this.sq9.Size = new System.Drawing.Size(53, 58);
+            this.sq9.TabIndex = 52;
+            this.sq9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sq8
+            // 
+            this.sq8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sq8.Enabled = false;
+            this.sq8.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.sq8.Location = new System.Drawing.Point(261, 162);
+            this.sq8.Name = "sq8";
+            this.sq8.Size = new System.Drawing.Size(53, 58);
+            this.sq8.TabIndex = 51;
+            this.sq8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnnew
+            // 
+            this.btnnew.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnnew.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnnew.Location = new System.Drawing.Point(6, 71);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(124, 25);
+            this.btnnew.TabIndex = 42;
+            this.btnnew.Text = "Start new game";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // sq6
+            // 
+            this.sq6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sq6.Enabled = false;
+            this.sq6.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.sq6.Location = new System.Drawing.Point(322, 95);
+            this.sq6.Name = "sq6";
+            this.sq6.Size = new System.Drawing.Size(53, 58);
+            this.sq6.TabIndex = 49;
+            this.sq6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sq5
+            // 
+            this.sq5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sq5.Enabled = false;
+            this.sq5.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.sq5.Location = new System.Drawing.Point(261, 95);
+            this.sq5.Name = "sq5";
+            this.sq5.Size = new System.Drawing.Size(53, 58);
+            this.sq5.TabIndex = 48;
+            this.sq5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sq4
+            // 
+            this.sq4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sq4.Enabled = false;
+            this.sq4.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.sq4.Location = new System.Drawing.Point(196, 95);
+            this.sq4.Name = "sq4";
+            this.sq4.Size = new System.Drawing.Size(53, 58);
+            this.sq4.TabIndex = 47;
+            this.sq4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sq3
+            // 
+            this.sq3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sq3.Enabled = false;
+            this.sq3.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.sq3.Location = new System.Drawing.Point(322, 25);
+            this.sq3.Name = "sq3";
+            this.sq3.Size = new System.Drawing.Size(53, 58);
+            this.sq3.TabIndex = 46;
+            this.sq3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sq2
+            // 
+            this.sq2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sq2.Enabled = false;
+            this.sq2.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.sq2.Location = new System.Drawing.Point(261, 25);
+            this.sq2.Name = "sq2";
+            this.sq2.Size = new System.Drawing.Size(53, 58);
+            this.sq2.TabIndex = 45;
+            this.sq2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sq7
+            // 
+            this.sq7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sq7.Enabled = false;
+            this.sq7.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.sq7.Location = new System.Drawing.Point(196, 162);
+            this.sq7.Name = "sq7";
+            this.sq7.Size = new System.Drawing.Size(53, 58);
+            this.sq7.TabIndex = 50;
+            this.sq7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sq1
+            // 
+            this.sq1.BackColor = System.Drawing.Color.Transparent;
+            this.sq1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sq1.Enabled = false;
+            this.sq1.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.sq1.Location = new System.Drawing.Point(196, 25);
+            this.sq1.Name = "sq1";
+            this.sq1.Size = new System.Drawing.Size(53, 58);
+            this.sq1.TabIndex = 44;
+            this.sq1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMsg.Location = new System.Drawing.Point(6, 185);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(124, 23);
+            this.lblMsg.TabIndex = 43;
+            this.lblMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.label1);
+            this.GroupBox1.Controls.Add(this.oplayer);
+            this.GroupBox1.Controls.Add(this.xplayer);
+            this.GroupBox1.Controls.Add(this.btnnew);
+            this.GroupBox1.Controls.Add(this.lblMsg);
+            this.GroupBox1.Location = new System.Drawing.Point(12, 12);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(136, 226);
+            this.GroupBox1.TabIndex = 41;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Select starting player";
+            // 
+            // oplayer
+            // 
+            this.oplayer.AutoSize = true;
+            this.oplayer.Location = new System.Drawing.Point(24, 48);
+            this.oplayer.Name = "oplayer";
+            this.oplayer.Size = new System.Drawing.Size(76, 17);
+            this.oplayer.TabIndex = 16;
+            this.oplayer.Text = "player 2: O";
+            this.oplayer.UseVisualStyleBackColor = true;
+            // 
+            // xplayer
+            // 
+            this.xplayer.AutoSize = true;
+            this.xplayer.Checked = true;
+            this.xplayer.Location = new System.Drawing.Point(24, 24);
+            this.xplayer.Name = "xplayer";
+            this.xplayer.Size = new System.Drawing.Size(75, 17);
+            this.xplayer.TabIndex = 16;
+            this.xplayer.TabStop = true;
+            this.xplayer.Text = "player 1: X";
+            this.xplayer.UseVisualStyleBackColor = true;
+            // 
+            // box
+            // 
+            this.box.Image = ((System.Drawing.Image)(resources.GetObject("box.Image")));
+            this.box.Location = new System.Drawing.Point(154, 12);
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(269, 226);
+            this.box.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.box.TabIndex = 40;
+            this.box.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Message:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 253);
+            this.Controls.Add(this.sq9);
+            this.Controls.Add(this.sq8);
+            this.Controls.Add(this.sq6);
+            this.Controls.Add(this.sq5);
+            this.Controls.Add(this.sq4);
+            this.Controls.Add(this.sq3);
+            this.Controls.Add(this.sq2);
+            this.Controls.Add(this.sq7);
+            this.Controls.Add(this.sq1);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.box);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.box)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label sq9;
+        internal System.Windows.Forms.Label sq8;
+        internal System.Windows.Forms.Button btnnew;
+        internal System.Windows.Forms.Label sq6;
+        internal System.Windows.Forms.Label sq5;
+        internal System.Windows.Forms.Label sq4;
+        internal System.Windows.Forms.Label sq3;
+        internal System.Windows.Forms.Label sq2;
+        internal System.Windows.Forms.Label sq7;
+        internal System.Windows.Forms.Label sq1;
+        internal System.Windows.Forms.Label lblMsg;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.RadioButton oplayer;
+        internal System.Windows.Forms.RadioButton xplayer;
+        internal System.Windows.Forms.PictureBox box;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Label[] squares;
+        public string player;
+        public int turn_number;
+        private bool isGameOver;
+
+        private static  int[,] Winners = new int[,]
+                                                    {
+
+                                                    {0,1,2},
+
+                                                    {3,4,5},
+
+                                                    {6,7,8},
+
+                                                    {0,3,6},
+
+                                                    {1,4,7},
+
+                                                    {2,5,8},
+
+                                                    {0,4,8},
+
+                                                    {2,4,6}
+
+                                                    };
+
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+          
+        }
+
+        private void init()
+        {
+            this.isGameOver = false;
+            turn_number = 0;
+            squares = new Label[9] { sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8, sq9 };
+            this.box.Enabled = false;
+            lblMsg.Text = "Click Start new game";
+            box.Enabled = false;
+            sq1.Enabled = false;
+            sq2.Enabled = false;
+            sq3.Enabled = false;
+            sq4.Enabled = false;
+            sq5.Enabled = false;
+            sq6.Enabled = false;
+            sq7.Enabled = false;
+            sq8.Enabled = false;
+            sq9.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                new_game();
+                for (int i = 0; i < 9; i++)
+                {
+                    this.squares[i].Click += new System.EventHandler(this.ClickHandler);
+                    this.squares[i].MouseMove += new MouseEventHandler(this.MouseMoveHandler);
+                    this.squares[i].MouseLeave += new System.EventHandler(this.MouseLeaveHandler);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void new_game()
+        {
+            this.box.Enabled = true;
+            lblMsg.Text = "";
+            turn_number = 0;
+            if (xplayer.Checked == true)
+            {
+                player = "x";
+                lblMsg.Text = "Player 1's turn";
+            }
+            else
+            {
+                player = "o";
+                lblMsg.Text = "Player 2's turn";
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+
+                squares[i].Text = "";
+
+                squares[i].ForeColor = Color.Black;
+
+                squares[i].BackColor = Color.Transparent;
+
+                squares[i].Enabled = true;
+            }
+        }
+
+        private void ClickHandler(object sender, System.EventArgs e)
+        {
+            Label tmp = (Label)sender;
+
+            turn_number += 1;
+
+            if (tmp.Text != "")
+            {
+                return;
+            }
+
+            if (player == "x")
+            {
+                tmp.ForeColor = Color.Red;
+                tmp.Text = "X";
+                player = "o";
+                lblMsg.Text = "Player 2's turn";
+                lblMsg.ForeColor = Color.Purple;
+            }
+            else
+            {
+                tmp.ForeColor = Color.Purple;
+                tmp.Text = "O";
+                player = "x";
+                lblMsg.Text = "Player 1's turn";
+                lblMsg.ForeColor = Color.Red;
+            }
+
+            tmp.BackColor = Color.Transparent;
+
+            this.isGameOver = CheckWinner(squares);
+
+            if (isGameOver)
+            {
+               
+                init();
+            }
+
+            if (turn_number == 9 && !isGameOver)
+            {
+                MessageBox.Show("Draw", "game over", MessageBoxButtons.OK);
+                lblMsg.Text = "";
+                init();
+            }
+        }
+
+
+
+        private void MouseMoveHandler(object sender, MouseEventArgs e)
+        {
+            Label tmp = (Label)sender;
+
+            if (tmp.Text != "")
+            {
+                return;
+            }
+
+            if (player == "x")
+            {
+                tmp.BackColor = Color.PowderBlue;
+            }
+            else
+            {
+                tmp.BackColor = Color.PeachPuff;
+            }
+        }
+
+        private void MouseLeaveHandler(object sender, EventArgs e)
+        {
+            Label tmp = (Label)sender;
+
+            if (tmp.Text != "")
+            {
+                return;
+            }
+            else
+            {
+                tmp.BackColor = Color.Transparent;
+            }
+
+        }
+
+        private static bool CheckWinner(Label[] ctrls)
+        {
+            
+            bool gameOver = false;
+
+            for (int i = 0; i < 8; i++)
+            {
+
+                int a = Winners[i, 0], b = Winners[i, 1], c = Winners[i, 2];
+
+                Label lb1 = ctrls[a], lb2 = ctrls[b], lb3 = ctrls[c];
+
+                if (lb1.Text == "" || lb2.Text == "" || lb3.Text == "")
+
+                    continue;
+
+                if (lb1.Text == lb2.Text && lb2.Text == lb3.Text)
+                {
+
+                    lb1.BackColor = lb2.BackColor = lb3.BackColor = Color.Silver;
+
+                    gameOver = true;
+
+                    MessageBox.Show("Player: " + lb1.Text + " wins ", "Game Over", MessageBoxButtons.OK);
+                    
+                    break;
+
+                }
+
+            }
+            
+            return gameOver;
+
+        }
+    }
+}
